5a7f60ff85dc1c223d1a1b59ada47308b035b8f4
tegarswasono <tegar.s@weefer.co.id>
2024-11-03 17:37:25 +0700

    try to create my own theme

--- LearnVuetify2/src/pages/features/theme.vue
+++ LearnVuetify2/src/pages/features/theme.vue
@@ -14,12 +14,31 @@ function toggleTheme () {
   theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark';
   localStorage.setItem('theme', theme.global.name.value);
 }
+function customTheme () {
+  theme.global.name.value = 'myCustomLightTheme';
+  localStorage.setItem('theme', theme.global.name.value);
+}
+function lightTheme () {
+  theme.global.name.value = 'light';
+  localStorage.setItem('theme', theme.global.name.value);
+}
 
 //if you want to apply to all page, you have to use a single v-app
 </script>
 
 <template>
   <v-card title="title" text="text blablabla"  />
-  <v-divider />
+  <v-divider class="my-5" />
+
   <v-btn @click="toggleTheme">Toggle theme</v-btn>
+  <v-btn @click="lightTheme">Light Theme</v-btn>
+  <v-btn @click="customTheme">Custom Theme</v-btn>
+  <v-divider class="my-5" />
+
+  <v-btn text="primary" color="primary" />
+  <v-btn text="secondary" color="secondary" />
+  <v-btn text="error" color="error" />
+  <v-btn text="info" color="info" />
+  <v-btn text="success" color="success" />
+  <v-btn text="warning" color="warning" />
 </template>
--- LearnVuetify2/src/plugins/vuetify.ts
+++ LearnVuetify2/src/plugins/vuetify.ts
@@ -9,11 +9,31 @@ import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 
 // Composables
-import { createVuetify } from 'vuetify'
+import { createVuetify, type ThemeDefinition } from 'vuetify'
+
+
+const myCustomLightTheme: ThemeDefinition = {
+  dark: false,
+  colors: {
+    background: '#FFFFFF',
+    surface: '#FFFFFF',
+    primary: '#6200EE',
+    'primary-darken-1': '#3700B3',
+    secondary: '#03DAC6',
+    'secondary-darken-1': '#018786',
+    error: '#FB8C00',
+    info: '#4CAF50',
+    success: '#2196F3',
+    warning: '#B00020',
+  },
+}
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
     defaultTheme: 'light',
+    themes: {
+      myCustomLightTheme
+    }
   },
 })
